1fef07d7ef7c20088a4cbddcc047d58d1e050ca5
wiloon <wiloon.wy@gmail.com>
2022-07-11 10:36:01 +0800

    django

--- foo.py
+++ foo.py
@@ -1,1 +1,20 @@
 print("Hello, World!")
+name = "hello world"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("\tpython\nfoo")
+
+# abc
+print("foo")
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+print(magician)
+
+for value in range(1, 5):
+    print(value)
